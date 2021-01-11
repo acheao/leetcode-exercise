9093244659b016574d3292422f58279610a44264
acheao <acheao@foxmail.com>
2021-01-11 22:15:13 +0800

    record Two Sum Solution

---
+++ src/main/java/Solution.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 作者：LeetCode-Solution
+ * 链接：https://leetcode-cn.com/problems/two-sum/solution/liang-shu-zhi-he-by-leetcode-solution/
+ * 来源：力扣（LeetCode）
+ * leetCode学习笔记 林超
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
+
+
+
